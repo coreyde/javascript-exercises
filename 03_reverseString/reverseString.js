f6c76c055d39f24b49fbcaded9bae6b951b772db
coreyde <corey_239@hotmail.com>
2024-09-12 09:05:12 +1000

    final

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+function reverseString(words) {
+  let result = "";
+  for (let i = words.length - 1; i > -1; i--) {
+    result += words[i];
+  }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
