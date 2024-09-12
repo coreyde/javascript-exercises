e58fa8407609034470bea1bf2efb04efcb73acc0
coreyde <corey_239@hotmail.com>
2024-09-12 12:01:05 +1000

    final

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...targets) {
+  result = array.filter((value) => !targets.includes(value));
+  return result;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
