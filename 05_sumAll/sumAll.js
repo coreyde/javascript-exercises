1a36c793c3c9a8db449d73fc0bf6207870ef04e7
coreyde <corey_239@hotmail.com>
2024-09-12 13:23:25 +1000

    final

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+function sumAll(value1, value2) {
+  result = 0;
+  if (
+    !Number.isInteger(value1) ||
+    value1 < 0 ||
+    !Number.isInteger(value2) ||
+    value2 < 0
+  ) {
+    return "ERROR";
+  } else if (value1 > value2) {
+    for (let i = value2; i < value1 + 1; i++) {
+      result += i;
+    }
+  } else {
+    for (let i = value1; i < value2 + 1; i++) {
+      result += i;
+    }
+  }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
